ff91bc92873188c61417e509b9eee716dafffd6b
Melina PM <meli.pm@gmail.com>
2022-10-15 20:01:48 -0300

    Agrego ruteo de nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { HouseCalendarComponent } from './components/house-calendar/house-calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'home', pathMatch:'full' },
+  { path:'home', component: HomeComponent},
+  { path: 'houses', component: HouseCalendarComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
